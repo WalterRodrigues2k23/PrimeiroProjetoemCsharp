461179bd5e28d4d682af0ebcdca44d87567e61f2
WALTER RODRIGUES DOS SANTOS JUNIOR <131314467+WalterRodrigues2k23@users.noreply.github.com>
2023-05-10 13:06:46 -0300

    Update Program.cs

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -8,12 +8,12 @@ namespace curso
         {
             Console.WriteLine("Entre com seu nome completo: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            Console.WriteLine("Qual sua idade? ");
             int a = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com o preço de um produto: ");
+            Console.WriteLine("digite o preço de um produto: ");
             double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha): ");
+            Console.WriteLine("Digite seu último nome, idade e altura (mesma linha): ");
             string[] vt = Console.ReadLine().Split(' ');
             Console.WriteLine();
             Console.WriteLine();
@@ -22,7 +22,7 @@ namespace curso
             double x = double.Parse(vt[2], CultureInfo.InvariantCulture);
 
             Console.WriteLine("Nome completo: " + name);
-            Console.WriteLine("Quartos na casa: " + a);
+            Console.WriteLine("Sua idade é: " + a);
             Console.WriteLine("Preço do produto: " + b.ToString(CultureInfo.InvariantCulture));
             Console.WriteLine("Seu último nome, idade e altura: " + nome2 + " " + z + " " + x.ToString("F2", CultureInfo.InvariantCulture));
             Console.ReadLine();
